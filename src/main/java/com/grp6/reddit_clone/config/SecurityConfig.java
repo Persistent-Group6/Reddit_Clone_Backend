38d18a63d874a3707ef13275cc8598c3c430e3c2
LilMonk <rahulsahoo66@gmail.com>
2021-04-25 18:17:18 +0530

    Server start issue solved

--- src/main/java/com/grp6/reddit_clone/config/SecurityConfig.java
+++ src/main/java/com/grp6/reddit_clone/config/SecurityConfig.java
@@ -22,8 +22,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService = null;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter();
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     @Override
